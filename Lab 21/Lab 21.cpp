9e460820c92fd381937dd8bff2f47f6797c2d38a
strwbrry1 <matvienkosemyon@gmail.com>
2023-12-01 23:25:59 +0400

    Добавьте файлы проекта.

---
+++ Lab 21/Lab 21.cpp
@@ -0,0 +1,404 @@
+﻿// Lab 21.cpp : Определяет точку входа для приложения.
+//
+
+#include "framework.h"
+#include "Lab 21.h"
+
+#define MAX_LOADSTRING 100
+#define N 10
+#define M 15
+#define WIDTH 30
+#define HEIGHT 20
+
+// Коды ячеек:
+// 0 - свободна
+// 1 - игрок
+// 2 - препятствие
+// 3 - золото
+int map[N][M] = {
+    {0, 0, 0, 0, 0,   0, 0, 0, 0, 0,   0, 0, 0, 0, 0},
+    {0, 1, 0, 0, 0,   0, 0, 0, 0, 3,   0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0,   0, 0, 0, 0, 3,   0, 0, 0, 0, 0},
+    {0, 0, 0, 3, 3,   3, 3, 0, 0, 0,   3, 3, 0, 0, 0},
+    {0, 0, 0, 0, 0,   0, 3, 0, 0, 0,   3, 3, 0, 0, 0},
+
+    {0, 0, 0, 0, 0,   0, 3, 3, 3, 0,   0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0,   0, 0, 0, 3, 0,   2, 0, 0, 2, 0},
+    {0, 0, 0, 0, 0,   0, 0, 2, 0, 0,   2, 0, 0, 2, 0},
+    {0, 0, 0, 0, 0,   0, 2, 2, 2, 2,   2, 2, 2, 2, 0},
+    {0, 0, 0, 0, 0,   0, 0, 0, 0, 0,   0, 0, 0, 0, 0}
+};
+
+int steps = 0;
+int gold = 0;
+
+void Left() {
+    for (int i = 0; i < N; i++) {
+        for (int j = 1; j < M; j++) {
+            if (map[i][j] == 1) { // если в map[i][j]  игрок
+                if (map[i][j - 1] == 0) { // если слева от игрока - пустая клетка
+                    map[i][j - 1] = 1;
+                    map[i][j] = 0;
+                    steps++;
+                }
+                else if (map[i][j - 1] == 3) {// если слева от игрока - золото
+                    map[i][j - 1] = 1;
+                    map[i][j] = 0;
+                    steps++;
+                    gold++;
+                }
+            }
+        }
+    }
+}
+
+void Right() {
+    for (int i = 0; i < N; i++) {
+        for (int j = M - 2; j >= 0; j--) {
+            if (map[i][j] == 1) { // если в map[i][j]  игрок
+                if (map[i][j + 1] == 0) {
+                    map[i][j + 1] = 1;
+                    map[i][j] = 0;
+                    steps++;
+                }
+                else if (map[i][j + 1] == 3) {
+                    map[i][j + 1] = 1;
+                    map[i][j] = 0;
+                    steps++;
+                    gold++;
+                }
+            }
+        }
+    }
+}
+
+void Up() {
+    for (int i = 1; i < N; i++) {
+        for (int j = M; j >= 0; j--) {
+            if (map[i][j] == 1) { // если в map[i][j]  игрок
+                if (map[i - 1][j] == 0) {
+                    map[i - 1][j] = 1;
+                    map[i][j] = 0;
+                    steps++;
+                }
+                else if (map[i - 1][j] == 3) {
+                    map[i - 1][j] = 1;
+                    map[i][j] = 0;
+                    steps++;
+                    gold++;
+                }
+            }
+        }
+    }
+}
+
+void Down() {
+    for (int i = N - 2; i >= 0; i--) {
+        for (int j = M; j >= 0; j--) {
+            if (map[i][j] == 1) { // если в map[i][j]  игрок
+                if (map[i + 1][j] == 0) {
+                    map[i + 1][j] = 1;
+                    map[i][j] = 0;
+                    steps++;
+                    break;
+                }
+                else if (map[i + 1][j] == 3) {
+                    map[i + 1][j] = 1;
+                    map[i][j] = 0;
+                    steps++;
+                    gold++;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+void insertGold() {
+    for (int i = N; i >= 0; i--) {
+        for (int j = M - 2; j >= 0; j--) {
+            if (map[i][j] == 1) { // если в map[i][j]  игрок
+                if (map[i][j + 1] == 0) {
+                    map[i][j + 1] = 3;
+                }
+            }
+        }
+    }
+}
+
+void insertWall() {
+    for (int i = N; i >= 0; i--) {
+        for (int j = M; j >= 1; j--) {
+            if (map[i][j] == 1) { // если в map[i][j]  игрок
+                if (map[i][j - 1] == 0) {
+                    map[i][j - 1] = 2;
+                }
+            }
+
+        }
+    }
+}
+
+void wallin() {
+    for (int i = N; i > 0; i--) {
+        for (int j = M; j >= 0; j--) {
+            if (map[i][j] == 1) { // если в map[i][j]  игрок
+                for (int y = i-1; y >= 0; y--) {
+                    map[y][j] = 2;
+                }    
+            }
+
+        }
+    }
+}
+
+void greed() {
+    for (int i = N - 1; i >= 0; i--) {
+        for (int j = M; j >= 0; j--) {
+            if (map[i][j] == 1) { // если в map[i][j]  игрок
+                map[i + 1][j] = 3;
+                if (j + 1 < M) {
+                    map[i + 1][j + 1] = 3;
+                }
+                if (j - 1 > 0) {
+                    map[i + 1][j - 1] = 3;
+                }
+            }
+
+        }
+    }
+}
+
+void deadlyLaser() {
+    for (int i = N; i >= 0; i--) {
+        for (int j = 0; j < M; j++) {
+            if (map[i][j] == 1) { // если в map[i][j]  игрок
+                for (int y = j + 1; y < M; y++) {
+                    map[i][y] = 2;
+                }
+            }
+
+        }
+    }
+}
+
+void drawMap(HDC hdc) {
+    // кисть для пустого поля
+    HBRUSH hBrushEmptyCell = CreateSolidBrush(RGB(200, 200, 200)); // серый
+    // кисть для поля с золотом
+    HBRUSH hBrushGold = CreateSolidBrush(RGB(255, 255, 0)); // желтый
+    // кисть для стены
+    HBRUSH hBrushWall = CreateSolidBrush(RGB(0, 0, 0)); // черный
+    // кисть для игрока
+    HBRUSH hBrushPlayer = CreateSolidBrush(RGB(0, 0, 255)); // синий
+
+    HBRUSH brush[4] = { hBrushEmptyCell, hBrushPlayer, hBrushWall, hBrushGold };
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+
+            int x1 = j * WIDTH;
+            int x2 = (j + 1) * WIDTH;
+            int y1 = i * HEIGHT;
+            int y2 = (i + 1) * HEIGHT;
+
+            RECT r = { x1, y1, x2, y2 };
+            FillRect(hdc, &r, brush[map[i][j]]);
+        }
+    }
+
+    for (int i = 0; i < 4; i++)
+        DeleteObject(brush[i]);
+}
+
+
+
+// Глобальные переменные:
+HINSTANCE hInst;                                // текущий экземпляр
+WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
+WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
+
+// Отправить объявления функций, включенных в этот модуль кода:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Разместите код здесь.
+
+    // Инициализация глобальных строк
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_LAB21, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Выполнить инициализацию приложения:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_LAB21));
+
+    MSG msg;
+
+    // Цикл основного сообщения:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_LAB21));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_LAB21);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // Разобрать выбор в меню:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_KEYDOWN:
+        switch (wParam)
+        {
+        case VK_DOWN:
+            Down();
+            InvalidateRect(hWnd, NULL, TRUE);
+            break;
+        case VK_LEFT:
+            Left();
+            InvalidateRect(hWnd, NULL, TRUE);
+            break;
+        case VK_UP:
+            Up();
+            InvalidateRect(hWnd, NULL, TRUE);
+            break;
+        case VK_RIGHT:
+            Right();
+            InvalidateRect(hWnd, NULL, TRUE);
+            break;
+        case 0x4c:
+            insertWall();
+            InvalidateRect(hWnd, NULL, TRUE);
+            break;
+        case 0x52:
+            insertGold();
+            InvalidateRect(hWnd, NULL, TRUE);
+            break;
+        case 0x55:
+            wallin();
+            InvalidateRect(hWnd, NULL, TRUE);
+            break;
+        case 0x44:
+            greed();
+            InvalidateRect(hWnd, NULL, TRUE);
+            break;
+        case 0x5a:
+            deadlyLaser();
+            InvalidateRect(hWnd, NULL, TRUE);
+            break;
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            drawMap(hdc);
+
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Обработчик сообщений для окна "О программе".
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
